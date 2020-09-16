c8a0e7ac02138f4da750b1f4cd6b66757b202925
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2020-09-16 21:02:04 +0200

    Copy gif source URLs to clipboard on click

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+function copySourceUrl(e) {
+  var caller = e.target || e.srcElement;
+
+  showSnackbar("copyMessage");
+
+  console.log( "Trying to write source URL to clipboard: " + caller.src );
+  navigator.clipboard.writeText( caller.src );
+}
+
+function showSnackbar(id) {
+  var x = document.getElementById( id );
+
+  x.className = "show";
+
+  setTimeout( function() { 
+    x.className = x.className.replace( "show", "" ); 
+  }, 2500 );
+}
