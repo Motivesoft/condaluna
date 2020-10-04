bc451d1503aef5f1409a9d57e735d12e0dbed2f9
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2020-10-04 21:51:42 +0200

    Loading flag for noscript images

--- assets/scripts/stickers.ps1
+++ assets/scripts/stickers.ps1
@@ -2,16 +2,6 @@
 # Sort it by file name
 $L = Import-Csv -Path .\assets\scripts\sticker-list.csv | Sort-Object -Property File
 
-# Write the list of img values for the no-script scenario in stickers.html
-Write-Output "** <noscript>"
-$L | ForEach-Object {
-    $File = $_.File
-    $Title = $_.Title
-    $Background = $_.Background
-
-    Write-Output "        <img class='noscript-card' style='background: $Background;' src='./$File' alt='$Title'/>"
-}
-
 # Write the list of image array entries for stickers.html
 Write-Output "** images[]"
 $L | ForEach-Object {
@@ -20,6 +10,16 @@ $L | ForEach-Object {
     $Background = $_.Background
 
     Write-Output "          [ './$File', '$Title', '$Background' ], "
+}
+
+# Write the list of img values for the no-script scenario in stickers.html
+Write-Output "** <noscript>"
+$L | ForEach-Object {
+    $File = $_.File
+    $Title = $_.Title
+    $Background = $_.Background
+
+    Write-Output "        <img class='noscript-card' loading='auto' style='background: $Background;' src='./$File' alt='$Title'/>"
 }
 
 # Write the list of image values for the stickers.html section in sitemap.xml
