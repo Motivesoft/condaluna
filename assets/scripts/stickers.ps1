1db67a46f1a8a501cd503b0ea6911f8b4184039d
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2020-10-09 13:24:09 +0200

    Auto-update stickers.html with stickers in assets folder

--- assets/scripts/stickers.ps1
+++ assets/scripts/stickers.ps1
@@ -3,6 +3,7 @@
 $L = Import-Csv -Path .\assets\scripts\sticker-list.csv | Sort-Object -Property File
 
 # Write the list of image array entries for stickers.html
+$imagesArray=""
 Write-Output "** images[]"
 Write-Output "        let images=["
 $L | ForEach-Object {
@@ -10,18 +11,21 @@ $L | ForEach-Object {
     $Title = $_.Title
     $Background = $_.Background
 
-    Write-Output "          [ './$File', '$Title', '$Background' ], "
+    #Write-Output "          [ './$File', '$Title', '$Background' ],"
+    $imagesArray = $imagesArray + "`r`n          [ './$File', '$Title', '$Background' ],"
 }
 Write-Output "        ];"
 
 # Write the list of img values for the no-script scenario in stickers.html
+$imagesNoscript=""
 Write-Output "** <noscript>"
 $L | ForEach-Object {
     $File = $_.File
     $Title = $_.Title
     $Background = $_.Background
 
-    Write-Output "        <img class='noscript-card' loading='auto' style='background: $Background;' src='./$File' title='$Title' alt='$Title'/>"
+    #Write-Output "        <img class='noscript-card' loading='auto' style='background: $Background;' src='./$File' title='$Title' alt='$Title'/>"
+    $imagesNoscript = $imagesNoscript + "`r`n        <img class='noscript-card' loading='auto' style='background: $Background;' src='./$File' title='$Title' alt='$Title'/>"
 }
 
 # Write the list of image values for the stickers.html section in sitemap.xml
@@ -35,3 +39,13 @@ $L | ForEach-Object {
     Write-Output "      <image:title>$Title</image:title>"
     Write-Output "    </image:image>"
 }
+
+#$x = "`r`n          ['x','#1'],`r`n          ['x','#1'],`r`n          ['x','#1'],`r`n          ['x','#1'],`r`n          ['x','#1'],"
+#$y = "`r`n        <img class='noscript-card'/>`r`n        <img class='noscript-card'/>`r`n        <img class='noscript-card'/>`r`n        <img class='noscript-card'/>"
+
+$content = [IO.File]::ReadAllText( '.\stickers.html' )
+
+$content = ($content -replace "(?ms)^\s+let images=\[.*\];", "        let images=[$imagesArray`r`n        ];")
+$content = ($content -replace "(?ms)^\s+<noscript id='stickers'>.*</noscript>", "      <noscript id='stickers'>$imagesNoscript`r`n      </noscript>")
+
+[IO.File]::WriteAllText('.\stickers.html',$content)
